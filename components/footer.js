4f8cf8e79ee6b8350e3ac41fc91c3ef5ff20be9d
Dmuse-project <danieluche702@gmail.com>
2025-09-18 11:03:50 +0100

    footer change

--- components/footer.js
+++ components/footer.js
@@ -4,7 +4,7 @@ import styles from './footer.module.css'
 export default function Footer(){
   return (
     <footer className={styles.footer}>
-      <div>© {new Date().getFullYear()} Photon Solar Nigeria</div>
+      <div>© {new Date().getFullYear()} Genesis-Eco power systems</div>
       <div className={styles.row}>
         <a href="tel:+2348073353857">+234 8073-353-857</a>
         <a href="mailto:lordmuse057@gmail.com">genesispowersystem@gmail.com</a>
